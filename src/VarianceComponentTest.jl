588e75bde1a679e3a297b748e4b81bda4cc7b691
Tao Hu <Tao-Hu@users.noreply.github.com>
2015-06-07 17:21:19 -0400

    Update install file after migrating from BitBucket

--- src/VarianceComponentTest.jl
+++ src/VarianceComponentTest.jl
@@ -1,7 +1,6 @@
 module VarianceComponentTest
 
 # package code goes here
-<<<<<<< HEAD
 using Distributions
 using Docile
 
@@ -13,7 +12,5 @@ include("vctest.jl")
 include("loopwinFixsize.jl")
 include("loopwinAnnot.jl")
 include("gwasvctest.jl")
-=======
->>>>>>> c05a9206c56a5f8814848f1d59841a2dfded52f5
 
 end # module
